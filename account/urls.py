ffad265ba1fd692f056e8ef762e72e34e6d33d54
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2024-02-05 20:13:30 +0600

    [Add] Templates

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from account import views
+
+app_name='account'
 
 urlpatterns = [
     # path('api/', include('account.urls'))
+    path('register/',views.registerRequest,name="register"),
 ]
