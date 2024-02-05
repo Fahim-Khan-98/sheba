045f1048f98471bbc4d68b9d60602db3732ad570
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2024-02-05 19:46:08 +0600

    [Add] Create User & SuperUser

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("username", "crimesection_admin", "entertainment_admin", "phone")
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('crimesection_admin', 'entertainment_admin', 'phone')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'crimesection_admin', 'entertainment_admin', 'phone'),
+        }),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,29 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from .managers import CustomUserManager
 
 # Create your models here.
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, crimesection_admin, entertainment_admin, phone, password=None, **extra_fields):
-        if not username:
-            raise ValueError('The username field must be set')
-
-        user = self.model(
-            username=username,
-            crimesection_admin=crimesection_admin,
-            entertainment_admin=entertainment_admin,
-            phone=phone,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, crimesection_admin, entertainment_admin, phone, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, crimesection_admin, entertainment_admin, phone, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True)
