0ad6946606e3db751cd32f804872aaf9db4266f6
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2024-02-09 21:10:52 +0600

    [Remove] Extra Lines

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -6,6 +6,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['id', 'user', 'first_name', 'last_name', 'avatar']
 
+
+
 class CustomUserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -20,27 +22,3 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'crimesection_admin', 'entertainment_admin', 'phone', 'is_active', 'is_staff', 'profile']
 
-    def create(self, validated_data):
-        profile_data = validated_data.pop('profile')
-        user = CustomUser.objects.create(**validated_data)
-        Profile.objects.create(user=user, **profile_data)
-        return user
-
-    def update(self, instance, validated_data):
-        profile_data = validated_data.pop('profile')
-        profile = instance.profile
-
-        instance.username = validated_data.get('username', instance.username)
-        instance.crimesection_admin = validated_data.get('crimesection_admin', instance.crimesection_admin)
-        instance.entertainment_admin = validated_data.get('entertainment_admin', instance.entertainment_admin)
-        instance.phone = validated_data.get('phone', instance.phone)
-        instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.is_staff = validated_data.get('is_staff', instance.is_staff)
-        instance.save()
-
-        profile.first_name = profile_data.get('first_name', profile.first_name)
-        profile.last_name = profile_data.get('last_name', profile.last_name)
-        profile.avatar = profile_data.get('avatar', profile.avatar)
-        profile.save()
-
-        return instance
