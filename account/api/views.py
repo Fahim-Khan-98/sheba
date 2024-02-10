94c81242632c1f7c8c6c287c05cada08ffca08dc
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2024-02-11 00:19:12 +0600

    add

--- account/api/views.py
+++ account/api/views.py
@@ -109,3 +109,6 @@ class ProfileRetrieveUpdateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+# Comment
